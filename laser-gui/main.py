4804f2f6b0837dade3975ecea36ce18b787895af
Alvin Tsui <awtsui@uci.edu>
2023-02-15 16:16:10 -0800

    integrated network scanner into gui

--- laser-gui/main.py
+++ laser-gui/main.py
@@ -1,8 +1,10 @@
 import tkinter
 import customtkinter
-import os
 from PIL import Image
 from CTkColorPicker import AskColor
+from scanner import scan_network
+
+customtkinter.set_appearance_mode("system")
 
 
 class App(customtkinter.CTk):
@@ -17,23 +19,25 @@ class App(customtkinter.CTk):
         self.title("LASERS")
         self.geometry("700x450")
 
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
         # FRAME DISCONNECTED
         self.frame_disconnect = customtkinter.CTkFrame(self)
         self.frame_disconnect.grid(row=0, column=0, sticky="nsew")
-        self.frame_disconnect.grid_rowconfigure(2, weight=1)
+        self.frame_disconnect.grid_rowconfigure(1, weight=1)
         self.frame_disconnect.grid_columnconfigure(0, weight=1)
 
         self.frame_disconnect_top = customtkinter.CTkFrame(self.frame_disconnect)
-        self.frame_disconnect_top.grid_columnconfigure(3, weight=1)
+        self.frame_disconnect_top.grid_columnconfigure((0, 1, 2, 3), weight=1)
         self.frame_disconnect_top.grid(row=0, column=0)
 
-        self.label_1 = customtkinter.CTkLabel(
-            master=self.frame_disconnect, text="LASERS"
+        self.label_name = customtkinter.CTkLabel(
+            master=self.frame_disconnect,
+            text="LASERS",
+            font=customtkinter.CTkFont("ITF Devanagari Marathi", 100, "normal"),
         )
-        self.label_1.grid(row=1, column=0, pady=10, padx=10)
+        self.label_name.grid(row=1, column=0, pady=10, padx=10)
 
         self.button_connect = customtkinter.CTkButton(
             master=self.frame_disconnect_top,
@@ -41,14 +45,14 @@ class App(customtkinter.CTk):
             text="Connect",
             state="disabled",
         )
-        self.button_connect.grid(row=0, column=2, pady=10, padx=10)
+        self.button_connect.grid(row=0, column=3, pady=10, padx=10)
 
         self.menu_lookup = customtkinter.CTkOptionMenu(
             master=self.frame_disconnect_top,
-            values=["IP Address Lookup", "192.168.0.253"],
+            values=["IP Address Lookup"],
             command=self.menu_lookup_callback,
         )
-        self.menu_lookup.grid(row=0, column=0, pady=10, padx=10)
+        self.menu_lookup.grid(row=0, column=1, pady=10, padx=10)
         self.menu_lookup.set("IP Address Lookup")
 
         self.entry_port = customtkinter.CTkEntry(
@@ -60,15 +64,23 @@ class App(customtkinter.CTk):
                 "%P",
             ),
         )
-        self.entry_port.grid(row=0, column=1, pady=10, padx=10)
+        self.entry_port.grid(row=0, column=2, pady=10, padx=10)
+
+        self.button_rescan = customtkinter.CTkButton(
+            master=self.frame_disconnect_top,
+            command=self.button_rescan_callback,
+            text="Scan Network",
+        )
+        self.button_rescan.grid(row=0, column=0, pady=10, padx=10)
 
         # FRAME CONNECTED
         self.frame_connect = customtkinter.CTkFrame(self)
         self.frame_connect.grid_columnconfigure(0, weight=1)
-        self.frame_connect.grid_rowconfigure(2, weight=1)
+        self.frame_connect.grid_rowconfigure(1, weight=1)
+        # self.frame_connect.grid(row=0, column=0, sticky="nsew")
 
         self.frame_connect_top = customtkinter.CTkFrame(self.frame_connect)
-        self.frame_connect_top.grid_columnconfigure(4, weight=1)
+        self.frame_connect_top.grid_columnconfigure((0, 1, 2, 3), weight=1)
         self.frame_connect_top.grid(row=0, column=0)
 
         self.button_disconnect = customtkinter.CTkButton(
@@ -95,8 +107,8 @@ class App(customtkinter.CTk):
         self.label_server_name.grid(row=0, column=3, pady=10, padx=10)
 
         self.frame_connect_bot = customtkinter.CTkFrame(self.frame_connect)
-        self.frame_connect_bot.grid_columnconfigure(2, weight=1)
-        self.frame_connect_bot.grid_rowconfigure(3, weight=1)
+        self.frame_connect_bot.grid_columnconfigure((0, 1), weight=1)
+        self.frame_connect_bot.grid_rowconfigure((0, 1, 2), weight=1)
         self.frame_connect_bot.grid(row=1, column=0)
 
         self.label_color = customtkinter.CTkLabel(
@@ -114,7 +126,7 @@ class App(customtkinter.CTk):
         self.button_color.grid(row=0, column=1, pady=10, padx=10)
 
         self.label_sensitivity = customtkinter.CTkLabel(
-            master=self.frame_connect_bot, text="Sensitivity"
+            master=self.frame_connect_bot, text="Sensitivity 50%"
         )
         self.label_sensitivity.grid(row=1, column=0, pady=10, padx=10)
 
@@ -128,7 +140,7 @@ class App(customtkinter.CTk):
         self.slider_sensitivity.set(0.5)
 
         self.label_brightness = customtkinter.CTkLabel(
-            master=self.frame_connect_bot, text="Brightness"
+            master=self.frame_connect_bot, text="Brightness 50%"
         )
         self.label_brightness.grid(row=2, column=0, pady=10, padx=10)
 
@@ -154,6 +166,16 @@ class App(customtkinter.CTk):
         print("Disconnecting...")
         self.switch_frames("disconnect")
 
+    def button_rescan_callback(self):
+        try:
+            self.button_rescan.configure(state="disabled")
+            result = scan_network()
+            devices = [device["ip"] for device in result]
+            self.menu_lookup.configure(values=["IP Address Lookup"] + devices)
+        except Exception as e:
+            print("Network scan failed: \n", e)
+        self.button_rescan.configure(state="normal")
+
     def menu_lookup_callback(self, choice):
         if choice == "IP Address Lookup":
             self.button_connect.configure(state="disabled")
@@ -167,9 +189,15 @@ class App(customtkinter.CTk):
 
     def slider_sensitivity_callback(self, value):
         print("Sensitivity slider value: ", value)
+        self.label_sensitivity.configure(
+            text="Sensitivity {:0.0f}%".format(100 * (value / 1.0))
+        )
 
     def slider_brightness_callback(self, value):
         print("Brightness slider value: ", value)
+        self.label_brightness.configure(
+            text="Brightness {:0.0f}%".format(100 * (value / 1.0))
+        )
 
     def entry_port_callback(self, value):
         if value.isnumeric():
@@ -184,7 +212,7 @@ class App(customtkinter.CTk):
 
     def button_color_callback(self):
         pick_color = AskColor()  # Open the Color Picker
-        color = pick_color.get()  # Get the color
+        color = pick_color.get() or "#ffffff"  # Get the color
         self.button_color.configure(fg_color=color)
         self.button_color.configure(text=color)
 
